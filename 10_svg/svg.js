4409044de680ae7952c119d5cad174c329628015
Shin Bamba <sbamba@stuy.edu>
2019-03-14 00:52:08 -0400

    not working

--- 10_svg/svg.js
+++ 10_svg/svg.js
@@ -9,26 +9,35 @@ var move = document.getElementById("move");
 
 
 
-pic.addEventListener('click', function(e){
-    // var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    
-    var x = e.offsetX;
-    var y = e.offsetY;
-    
-    console.log("starting");
-    c.setAttribute( "cx", x);
-    c.setAttribute( "cy", y);
-    c.setAttribute( "r", 10);	
-    c.setAttribute( "stroke", "black");
-    c.setAttribute( "fill", "blue");
-    pic.appendChild(c)
 
-    c.addEventListener('click', function(e){
-	c.setAttribute( "fill", "green");
-	console.log("clicked");
-    });
+pic.addEventListener('click', function(e){
+	e.preventDefault();
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	
+	var x = e.offsetX;
+	var y = e.offsetY;
+   
+	console.log("starting");
+	c.setAttribute( "cx", x);
+	c.setAttribute( "cy", y);
+	c.setAttribute( "r", 10);	
+	c.setAttribute( "stroke", "black");
+	c.setAttribute( "fill", "blue");
+	
+	// console.log(c.getAttribute('fill'));
+	pic.appendChild(c)
+
+   
 });
+var cir = function(c){
+	c.addEventListener('click', function(e){
+		console.log(c);
+		
+		if(c.getAttribute('fill') == 'blue'){
+			c.setAttribute('fill', 'green');
+		}
+	});
+}
 
 
 
