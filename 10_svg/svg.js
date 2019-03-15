fa30bd59a0b43a314c66700c388bd45f52307899
Shin Bamba <sbamba@stuy.edu>
2019-03-15 01:09:11 -0400

    not working

--- 10_svg/svg.js
+++ 10_svg/svg.js
@@ -5,49 +5,65 @@ var move = document.getElementById("move");
 // var l = document.createElementNS("http://www.w3.org/2000/svg", "line");
 
 
+var first = true; // checks if dot exists
 
 
 
 
-
-pic.addEventListener('click', function(e){
-	e.preventDefault();
-    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-	
-	var x = e.offsetX;
-	var y = e.offsetY;
+// var p = function(x,y) {
+	// p.addEventListener('click', function(e){
    
-	console.log("starting");
-	c.setAttribute( "cx", x);
-	c.setAttribute( "cy", y);
-	c.setAttribute( "r", 10);	
-	c.setAttribute( "stroke", "black");
-	c.setAttribute( "fill", "blue");
+	// console.log("starting");
+	// c.setAttribute( "cx", x);
+	// c.setAttribute( "cy", y);
+	// c.setAttribute( "r", 10);	
+	// c.setAttribute( "stroke", "black");
+	// c.setAttribute( "fill", "blue");
 	
 	// console.log(c.getAttribute('fill'));
-	pic.appendChild(c)
-
+	// p.appendChild(c)
    
-});
-var cir = function(c){
-	c.addEventListener('click', function(e){
-		console.log(c);
-		
-		if(c.getAttribute('fill') == 'blue'){
-			c.setAttribute('fill', 'green');
-		}
-	});
+	// });
+// }
+
+var circle = function(x,y){
+	console.log("drawing cirlce");
+	var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	c.setAttribute("cx",x);
+    c.setAttribute("cy",y);
+    c.setAttribute("r","10");
+    c.setAttribute("fill","blue");
+    c.addEventListener('click',change(c))
+    pic.appendChild(e,c);
+};
+
+var change = function(c){
+	console.log("change");
+	e.preventDefault();
+	e.stopPropagation();
+	if(c.getAtrribute("fill") = "blue"){
+		c.setAttribute('fill','green')
+		console.log("changinng to green");
+	}
+	else if(this.getAttribute('fill')='green'){
+		xcor= Math.floor( Math.random() * pic.getAttribute("width"));
+        ycor= Math.floor( Math.random() * pic.getAttribute("height"));
+        pic.removeChild(this);
+        circle( xcor, ycor );
+		console.log("disappear");
+	};
 }
 
-
-
-
-
+pic.addEventListener('click', function(e){
+	var x = e.offsetX;
+	var y = e.offsetY;
+	circle(x,y);
+});
 clear.addEventListener('click', function(e){
     e.preventDefault();
     // console.log(e);
-	
-    while (pic.lastChild) {// loop through svg deleting all children
-	pic.removeChild(pic.lastChild);
-    }
+	pic.innerHTML = "";
+    // while (pic.lastChild) {// loop through svg deleting all children
+	// pic.removeChild(pic.lastChild);
+    // }
  });
