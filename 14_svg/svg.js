25ae76e2c405c9bc321a5ceb8ec8a830c5394d31
Shin Bamba <sbamba@stuy.edu>
2019-04-04 23:53:25 -0400

    p4 work

--- 14_svg/svg.js
+++ 14_svg/svg.js
@@ -1,21 +1,31 @@
-var width = 420,
-    barHeight = 20;
-
-var x = d3.scale.linear().range([0, width]);
-
-var chart = d3.select(".chart").attr("width", width);
-
-d3.tsv("data.tsv", type, function(error, data) {
-    x.domain([0, d3.max(data, function(d) { return d.value; })]);
-    chart.attr("height", barHeight * data.length);
-    var bar = chart.selectAll("g").data(data)
-                    .enter().append("g").attr("transform", function (d, i) {
-                                                            return "translate(0," + i * barHeight + ")"; });
-    bar.append("rect").attr("width", function (d) {
-                                        return x(d.value);})
-                        .attr("height", barHeight - 1);
-    bar.append("text").attr("x", function (d){
-                                    return x(d.value) - 3;})
-                        .attr("y", barHeight / 2)
-                        .attr("dy", ".35em")
-                        .text(function(d) { return d.value; });
+ var selection = d3.select("body");
+      d3.selectAll("h2");
+      d3.selectAll("tr").selectAll("td");
+      d3.selectAll("tr").selectAll("td").selectAll("span");
+      d3.selectAll("section");
+      d3.selectAll("section").append("p");
+      d3.selectAll("section").select("aside");
+      d3.select("body").datum(42);
+      d3.select("body").datum(42).append("h1");
+      var numbers = [4, 5, 18, 23, 42];
+      d3.selectAll("div").data(numbers);
+      var letters = [
+        {name: "A", frequency: .08167},
+        {name: "B", frequency: .01492},
+        {name: "C", frequency: .02780},
+        {name: "D", frequency: .04253},
+        {name: "E", frequency: .12702}
+      ];
+      function name(d) {
+        return d.name;
+      }
+      d3.selectAll("div").data(letters, name);
+      var matrix = [
+        [ 0,  1,  2,  3],
+        [ 4,  5,  6,  7],
+        [ 8,  9, 10, 11],
+        [12, 13, 14, 15]
+        ];
+      var div = d3.selectAll("div").data(vowels, name);
+      div.exit();
+      div.enter();
